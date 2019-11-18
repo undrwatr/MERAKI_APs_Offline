8efce821260b705b477f5eec9f21410d11ebd48d
Joseph Jenkins <jjenkins@bmontez-lt.hottopic.com>
2019-11-18 09:32:55 -0800

    finished pylint update to program

--- APs_offline.py
+++ APs_offline.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-
-
-#Program to audit the store network and look for APs that are offline
+"""Program to audit the store network and look for APs that are offline,
+based on the S/N of the device. This way it will ignore other devices in the network"""
 
 #imports
 import re
@@ -11,35 +10,34 @@ import requests
 import cred
 
 #This pulls the information based on s/ns that start with these.
-regex = re.compile('Q2PD')
-regex1 = re.compile('Q2HD')
+REGEX = re.compile('Q2PD')
+REGEX1 = re.compile('Q2HD')
 
 #custom variables for the program imported from the cred.py file located in the same directory
-organization = cred.organization
-key = cred.key
-hub = cred.hub
+ORGANIZATION = cred.organization
+KEY = cred.key
 
 #Main URL for the Meraki Platform
-dashboard = "https://dashboard.meraki.com"
+DASHBOARD = "https://dashboard.meraki.com"
 #api token and other data that needs to be uploaded in the header
-headers = {'X-Cisco-Meraki-API-Key': (key), 'Content-Type': 'application/json'}
+HEADERS = {'X-Cisco-Meraki-API-Key': (KEY), 'Content-Type': 'application/json'}
 
 #Pull back all of the network IDs:
-get_network_url = dashboard + '/api/v0/organizations/%s/networks' % organization
-get_network_response = requests.get(get_network_url, headers=headers)
-get_network_json = get_network_response.json()
+GET_NETWORK_URL = DASHBOARD + '/api/v0/organizations/%s/networks' % ORGANIZATION
+GET_NETWORK_RESPONSE = requests.get(GET_NETWORK_URL, headers=HEADERS)
+GET_NETWORK_JSON = GET_NETWORK_RESPONSE.json()
 
 #pull back the status of all devices
-get_dev_sts_url = dashboard + '/api/v0/organizations/%s/deviceStatuses' % organization
-get_dev_sts_rsp = requests.get(get_dev_sts_url, headers=headers)
-get_dev_sts_json = get_dev_sts_rsp.json()
+GET_DEV_STS_URL = DASHBOARD + '/api/v0/organizations/%s/deviceStatuses' % ORGANIZATION
+GET_DEV_STS_RSP = requests.get(GET_DEV_STS_URL, headers=HEADERS)
+GET_DEV_STS_JSON = GET_DEV_STS_RSP.json()
 #print(get_dev_sts_json)
 
-for z in get_dev_sts_json:
+for z in GET_DEV_STS_JSON:
     if z["status"] == "offline":
         down_id = z["networkId"]
-        for i in get_network_json:
-            if down_id == i["id"] and regex.match(z["serial"]):
+        for i in GET_NETWORK_JSON:
+            if down_id == i["id"] and REGEX.match(z["serial"]):
                 print(i["name"] + ' ' + z["serial"])
-            if down_id == i["id"] and regex1.match(z["serial"]):
+            if down_id == i["id"] and REGEX1.match(z["serial"]):
                 print(i["name"] + ' ' + z["serial"])
